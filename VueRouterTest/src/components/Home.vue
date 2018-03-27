9a20338354ee2c58cdb0d94a33168691a69540b7
wangwyatt <wang_wyatt@outlook.com>
2018-03-27 20:44:20 +0800

    增加Home.vue

---
+++ VueRouterTest/src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <Carousel autoplay>
+      <Carousel-item>
+        <div class="demo-carousel">
+          <img src="http://www.cnidea.net/toutiao/u/20161231/163651156016069525738.jpg" alt=""/>
+        </div>
+      </Carousel-item>
+  </Carousel>
+</template>
